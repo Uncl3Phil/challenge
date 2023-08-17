8737d47689ec37ebe2700285c7c07a612c9d0d6c
Philip <philip.marques@vibryt.com.br>
2023-08-17 18:13:55 -0300

    feat: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: ["./src/**/*.{html,js}"],
+   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        "2xl": "2rem",
+      },
+    },
+    extend: {
+      screens: {
+        laptop: { max: "1440px" },
+        mobile: { max: "375px" },
+      },
+      colors: {
+        primary: "hsl(4, 100%, 67%)",
+        darkGrey: "hsl(234, 29%, 20%)",
+        mediumGrey: "hsl(235, 18%, 26%)",
+        grey: "hsl(231, 7%, 60%)",
+      },
+    },
+  },
+  plugins: [],
+}
+
